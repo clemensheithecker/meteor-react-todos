830f6b2b3a2ba98c4a5f65862088928a243095de
Clemens Heithecker <clemens@heithecker.dev>
2022-11-08 16:30:04 +0100

    Render tasks collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,19 @@
 import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { TasksCollection } from "/imports/api/TasksCollection.js";
 import { Task } from "./Task.jsx";
 
-// The underscore identifier _ is used to preface names of object properties that are private.
-const tasks = [
-  { _id: 1, text: "Clean the house" },
-  { _id: 2, text: "Buy groceries" },
-  { _id: 3, text: "Walk the dog" },
-];
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
-export const App = () => (
-  <section>
-    <h1>To-Dos</h1>
-    <ul>
-      {tasks.map((task) => (
-        <Task key={task._id} task={task} />
-      ))}
-    </ul>
-  </section>
-);
+  return (
+    <section>
+      <h1>To-Dos</h1>
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+    </section>
+  );
+};
