48d26fc2098a3eaeedb91fc09811a11cc2117bb2
Clemens Heithecker <clemens@heithecker.dev>
2022-11-08 19:20:47 +0100

    Update CSS styles

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -14,6 +14,14 @@ const toggleChecked = ({ _id, isChecked }) => {
 
 const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
+const convertTime = (time) => {
+  return new Date(time).toLocaleDateString("en-us", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  });
+};
+
 export const App = () => {
   const tasks = useTracker(() =>
     // Show newest tasks first
@@ -21,21 +29,29 @@ export const App = () => {
   );
 
   return (
-    <section>
-      <h1>To-Dos</h1>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>To-Dos</h1>
+            <p>{convertTime(new Date())}</p>
+          </div>
+        </div>
+      </header>
+      <main className="main">
+        <TaskForm />
 
-      <TaskForm />
-
-      <ul>
-        {tasks.map((task) => (
-          <Task
-            key={task._id}
-            task={task}
-            onCheckboxClick={toggleChecked}
-            onDeleteClick={deleteTask}
-          />
-        ))}
-      </ul>
-    </section>
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 };
