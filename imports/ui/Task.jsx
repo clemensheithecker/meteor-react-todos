98f07ee299df93cc16e8ab6d4bfccb5d1aa702dc
Clemens Heithecker <clemens@heithecker.dev>
2022-11-08 17:53:14 +0100

    Add checkbox

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export const Task = ({ task }) => {
-  return <li>{task.text}</li>;
+export const Task = ({ task, onCheckboxClick }) => {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        // Force checked prop to a boolean
+        checked={!!task.isChecked}
+        onClick={() => onCheckboxClick(task)}
+        // Adding the readOnly attribute since we are not using onChange to
+        // update the state
+        readOnly
+      />
+      <span>{task.text}</span>
+    </li>
+  );
 };
